58d15fbdadb492f5ba15073ec5c3f740ec887874
Moritz Waldleben <moritz.waldleben@gmail.com>
2022-01-13 23:25:19 +0100

    report still not finished

--- err_analysis/results/plots.m
+++ err_analysis/results/plots.m
@@ -1,29 +1,26 @@
-clear all
+clear all;
 
-hmaxs_conf = fftomatlab_vector(’hmaxs_conf.dat’);
-errH1_conf = fftomatlab_vector(’errH1_conf.dat’);
-errL2_conf = fftomatlab_vector(’errL2_conf.dat’);
+h = fftomatlab_vector('hmaxs_conf.dat');
+errH1_conf = fftomatlab_vector('errH1_conf.dat');
+errL2_conf = fftomatlab_vector('errL2_conf.dat');
 
-hmaxs_nonconf = fftomatlab_vector(’hmaxs_nonconf.dat’);
-errH1_nonconf = fftomatlab_vector(’errH1_nonconf.dat’);
-errL2_nonconf = fftomatlab_vector(’errL2_nonconf.dat’);
+h2 = fftomatlab_vector('hmaxs_nonconf.dat');
+errH1_nonconf = fftomatlab_vector('errH1_nonconf.dat');
+errL2_nonconf = fftomatlab_vector('errL2_nonconf.dat');
 
-constH1 = 3e0;
-boundH1 = constL2 * h;
+boundH1 = h + 5;
 
-constL2 = 3e0;
-boundL2 = constL2 * h;
+boundL2 = h + 0.001;
 
-tiledlayout(2,1)
-nexttile
-loglog(h, errH1_conf, ’-x’, h, errH1_nonconf, ’-o’, h, boundL2); 
-legend(’conformal mesh’, ’mortar’, ’h’, ’Location’, ’Best’);
-xlabel('H1 seminorm of u_ref - u_h') 
-ylabel('h') 
+figure(1)
+loglog(h, errH1_conf, '-x', h2, errH1_nonconf, '-o', h, boundH1); 
+legend('conformal mesh', 'mortar', 'h', 'Location', 'Best');
+xlabel('h') 
+ylabel('H1 seminorm of u_h - u_{ref}') 
 
-nexttile
-loglog(h, errL2_conf, ’-x’, h, errL2_nonconf, ’-o’, h, boundL2); 
-legend(’conformal mesh’, ’mortar’, ’h’, ’Location’, ’Best’);
-xlabel('L2 norm of u_ref - u_h') 
-ylabel('h') 
+figure(2)
+loglog(h, errL2_conf, '-x', h2, errL2_nonconf, '-o', h, boundL2); 
+legend('conformal mesh', 'mortar', 'h', 'Location', 'Best');
+xlabel('h') 
+ylabel('L2 norm of u_h - u_{ref}') 
 
