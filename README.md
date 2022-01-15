0d96113387e0011cb8a3be6650d8569efa0913bf
Moritz Waldleben <moritz.waldleben@gmail.com>
2022-01-15 09:47:00 +0100

    final version

---
+++ README.md
@@ -0,0 +1,2 @@
+# Project, Numerics for fluids, structures and electromagnetics
+Mathematical analysis and implementation in FreeFEM++ of a Stokes flow in a lid driven cavity.  
--- err_analysis/plots/plots.m
+++ err_analysis/plots/plots.m
@@ -1,6 +1,6 @@
 clear all;
 
-n2 = fftomatlab_vector('hmaxs_conf.dat');
+n2 = fftomatlab_vector('n2_conf.dat');
 errH1_conf = fftomatlab_vector('errH1_conf.dat');
 errL2_conf = fftomatlab_vector('errL2_conf.dat');
 
@@ -8,14 +8,13 @@ errH1_nonconf = fftomatlab_vector('errH1_nonconf.dat');
 errL2_nonconf = fftomatlab_vector('errL2_nonconf.dat');
 
 figure(1)
-loglog(n2, errH1_conf, '-x', n2, errH1_nonconf, '-o'); 
-legend('conformal mesh', 'mortar',, 'Location', 'Best');
+plot(n2, errH1_conf, '-x', n2, errH1_nonconf, '-o'); 
+legend('conformal mesh', 'mortar', 'Location', 'Best');
 xlabel('N2') 
 ylabel('H1 seminorm of u_h - u_{ref}') 
 
 figure(2)
-loglog(n2, errL2_conf, '-x', n2, errL2_nonconf, '-o', h, boundL2); 
+plot(n2, errL2_conf, '-x', n2, errL2_nonconf, '-o'); 
 legend('conformal mesh', 'mortar', 'Location', 'Best');
 xlabel('N2') 
 ylabel('L2 norm of u_h - u_{ref}') 
-
